2f972768c8a722b69d201b2e668437bb711d2228
nxtmalteser <BernardBorg@Bernards-MacBook-Pro.local>
2014-10-18 23:46:32 +0200

    PFABOnline

---
+++ homeworlk1.py
@@ -0,0 +1,33 @@
+import math
+#homework part 1
+#var
+last = "Borg"
+first = "Bernard"
+age = 31
+
+#output
+print first, last, age
+
+#homework part 2
+
+sideA = 12.55
+sideB = 17.85
+
+sideC = (sideA * sideA) + (sideB * sideB)
+pyF = math.sqrt(sideC)
+
+print "sideC is equal to", pyF #output
+
+#homework part 3
+
+operand1 = 95
+operand2 = 64.5
+
+print operand1 + operand2
+print operand1 - operand2
+print operand1 * operand2
+print operand1 / operand2
+print operand1 % operand2
+
+# change of variable value would result in an immidiate change in the output of all the funcions
+
